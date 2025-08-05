4056db09dda3b9ce33f10a82679955364076a774
GrischaE1 <54313015+GrischaE1@users.noreply.github.com>
2025-08-05 08:41:44 +0200

    Update WorkspaceONE_ReEnrollment.ps1

--- Scripts/WorkspaceONE_ReEnrollment.ps1
+++ Scripts/WorkspaceONE_ReEnrollment.ps1
@@ -32,91 +32,195 @@ if ($Reenrolldevice -eq "true") {
     $WebClient.DownloadFile("https://$($DSServerURL)/agents/ProtectionAgent_autoseed/airwatchagent.msi", "C:\temp\AirwatchAgent.msi")
 }
 
+# Get Enrollment ID
+    try {
+        # Retrieve all items under the Enrollment registry key
+        $AllItems = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Enrollments -Recurse -ErrorAction Stop
+        $AirWatchMDMKey = $AllItems | Where-Object { $_.Name -like "*AirWatchMDM" }
+
+        # Ensure the AirWatchMDMKey exists
+        if (-not $AirWatchMDMKey) {
+            throw "No AirWatchMDM key found in the registry."
+        }
+
+        # Extract the Enrollment Key using regex
+        $pattern = "Enrollments\\(.*?)\\DMClient"
+        $EnrollmentKey = ([regex]::Match(($AirWatchMDMKey.PSPath), $pattern).Groups[1].Value).Replace("\\", "")
+
+        if (-not $EnrollmentKey) {
+            throw "Failed to extract Enrollment Key using the specified regex pattern."
+        }
+
+    }
+    catch {
+    }
+
+    
+    # Uninstall SFD to avoid application uninstallation
+    try {
+        # Retrieve the SFD Agent registry entry
+        $Registry = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall -ErrorAction Stop | Where-Object { $_.GetValue('DisplayName') -like "*SfdAgent*" }
+
+        # Ensure the registry entry exists
+        if (-not $Registry) {
+            throw "SFD Agent registry entry not found."
+        }
+
+        # Construct the uninstall command
+        $SFDUninstall = "/x $($Registry.PSChildName) /q"
+
+        # Execute the uninstall process
+        Start-Process MsiExec.exe -ArgumentList $SFDUninstall -Wait -ErrorAction Stop
+    }
+    catch {
+    }
 
 
-#Get Enrollment ID
-$AllItems = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Enrollments -Recurse
-$AirWatchMDMKey = $AllItems | Where-Object { $_.Name -like "*AirWatchMDM" }
-$pattern = "Enrollments(.*?)\DMClient"
-$EnrollmentKey = ([regex]::Match(($AirWatchMDMKey.PSPath), $pattern).Groups[1].Value).Replace("\", "")
+    # Remove SFD and OMA-DM Registry keys
+    try {
+        # Remove Registry Keys
+        $registryKeys = @(
+            "HKLM:\\SOFTWARE\\Microsoft\\EnterpriseDesktopAppManagement",
+            "HKLM:\\SOFTWARE\\AirWatchMDM"
+        )
+
+        foreach ($key in $registryKeys) {
+            try {
+                if (Test-Path $key) {
+                    Remove-Item $key -Recurse -Force -ErrorAction Stop
+                }
+            }
+            catch {
+            }
+        }
+    }
+    catch {
+    }
+
+    # Uninstall Intelligent Hub
+    if ($enrollmentStatus.WorkspaceONEInstalled -eq $True) {
+        try {
+            # Retrieve Intelligent Hub installation data
+            $HubData = Get-WmiObject Win32_Product -ErrorAction Stop | Where-Object { $_.Name -like "*Intelligent HUB Installer*" }
+
+            # Validate if Intelligent Hub is found
+            if ($HubData) {
+                # Construct the uninstall command
+                $HubUninstall = "/x $($HubData.IdentifyingNumber) /q"
+
+                # Execute the uninstall process
+                Start-Process MsiExec.exe -ArgumentList $HubUninstall -Wait -ErrorAction Stop
+            }
+        }
+        catch {
+        }
 
 
-#Uninstall SFD Agent
-if ($KeepAppsInstalled -eq "true") {
-    $SFDAgent = Get-WmiObject -Class win32_product -Filter "Name like '%SFD%'"
-    $Arguments = "/x $($SFDAgent.IdentifyingNumber) /q /norestart"
-    Start-Process msiexec -ArgumentList $Arguments -Wait 
-}
+        #Sleep for 60 seconds to make sure Hub is uninstalled
+        Start-Sleep -Seconds 60
 
-#Uninstall Agent - requires manual delete of device object in console
-$HUB = Get-WmiObject -Class win32_product -Filter "Name like '%Workspace ONE Intelligent%'"
-$Arguments = "/x $($HUB.IdentifyingNumber) /q /norestart"
-Start-Process msiexec -ArgumentList $Arguments -Wait 
+        # Uninstall WS1 App
+        try {
+            # Retrieve the WS1 app package
+            $WS1App = Get-AppxPackage *AirWatchLLC* -ErrorAction SilentlyContinue
 
-Start-Sleep -Seconds 120
+            # Validate if the app package exists
+            if ($WS1App) {
+                # Attempt to remove the app package
+                $WS1App | Remove-AppxPackage -ErrorAction SilentlyContinue
+            }
+        }
+        catch {    
+        }
+    }
 
-#uninstall WS1 App
-Get-AppxPackage *AirWatchLLC* | Remove-AppxPackage 
- 
-#Delete reg keys
-Remove-Item -Path HKLM:\SOFTWARE\Airwatch -Recurse -Force
-Remove-Item -Path HKLM:\SOFTWARE\AirwatchMDM -Recurse -Force
+    # Remove Enrollment Registry Keys
+    $registryKeys = @(
+        "HKLM:\SOFTWARE\AirWatch",
+        "HKLM:\SOFTWARE\Microsoft\Enrollments",
+        "HKLM:\SOFTWARE\Microsoft\EnterpriseDesktopAppManagement\S-0-0-00-0000000000-0000000000-000000000-000\MSI",
+        "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Accounts",
+        "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\logger",
+        "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\MDMDeviceID",
+        "HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked\$($EnrollmentKey)",
+        "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxDefault\$($EnrollmentKey)",
+        "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxInstalled\$($EnrollmentKey)",
+        "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\_ContainerAdmxDefault\*",
+        "HKLM:\SOFTWARE\Microsoft\PolicyManager\device\ApplicationManagement\*",
+        "HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers\$($EnrollmentKey)",
+        "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Session\$($EnrollmentKey)",
+        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsAnytimeUpgrade\Attempts\*"
+        "HKLM:\SOFTWARE\WorkspaceONE"
+        "HKLM:\SOFTWARE\AirWatchMDMBackup"
+        "HKLM:\SOFTWARE\VMware, Inc.\VMware EUC"
+        "HKLM:\SOFTWARE\VMware, Inc.\VMware Endpoint Telemetry"
+
+    )
+
+    foreach ($key in $registryKeys) {
+        try {
+            if (Test-Path $key) {
+                Remove-Item -Path $key -Recurse -Force -ErrorAction SilentlyContinue
+            }
+        }
+        catch {
+        }
+    }
 
 
 
-#Remove enrollment specific entries
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Enrollments\$($EnrollmentKey)" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked\$($EnrollmentKey)" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxDefault\$($EnrollmentKey)" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxInstalled\$($EnrollmentKey)" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\current\_ContainerAdmxDefault\*" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\device\ApplicationManagement\*" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers\$($EnrollmentKey)" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Session\$($EnrollmentKey)" -Recurse -Force
-Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsAnytimeUpgrade\Attempts\*" -Recurse -Force
+    #Delete folders
+    $directorypaths = @(
+        "$env:ProgramData\AirWatch",
+        "$env:ProgramData\VMware\SfdAgent",
+        "$env:ProgramFiles\WorkspaceONE",
+        "$env:ProgramData\VMware\vmwetlm",
+        "$env:ProgramData\VMware\EUC",
+        "$env:ProgramData\WorkspaceONE"
+    )
 
+    foreach ($directory in $directorypaths) {
+        try {
+            if (Test-Path $directory) {
+                Remove-Item $directory -Recurse -Force -ErrorAction SilentlyContinue
+            }
+        }
+        catch {           
+        }
+    }
 
+    #Clean Scheduled Tasks - SFD
+    Get-ScheduledTask -TaskPath "\Microsoft\Windows\EnterpriseMgmt\$($EnrollmentKey)\*" | Unregister-ScheduledTask  -Confirm:$false
 
-#Cleanup other registry
-Remove-Item -Path HKLM:\SOFTWARE\Microsoft\Provisioning\omadm\Accounts\* -Recurse -Force
-Remove-Item -Path HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Logger\* -Recurse -Force
-Remove-Item -Path HKLM:\SOFTWARE\Microsoft\EnterpriseDesktopAppManagement\*\MSI\* -Recurse -Force
- 
-#Delete folders
-$path = "$env:ProgramData\AirWatch"
-Remove-Item $path -Recurse -Force
+    $scheduleObject = New-Object -ComObject Schedule.Service
+    $scheduleObject.connect()
+    $rootFolder = $scheduleObject.GetFolder("\Microsoft\Windows\EnterpriseMgmt")
+    $rootFolder.DeleteFolder("$($EnrollmentKey)", $null)
 
-$path = "$env:ProgramData\AirWatchMDM"
-Remove-Item $path -Recurse -Force
+    #Clean Scheduled Tasks - SFD - before 24.10
+    Get-ScheduledTask -TaskPath "\vmware\SfdAgent\*" | Unregister-ScheduledTask  -Confirm:$false
 
-$path = "$env:ProgramData\VMware\SfdAgent"
-Remove-Item $path -Recurse -Force
+    $scheduleObject = New-Object -ComObject Schedule.Service
+    $scheduleObject.connect()
+    $rootFolder = $scheduleObject.GetFolder("\vmware")
+    $rootFolder.DeleteFolder("SfdAgent", $null)
 
+    #Clean Scheduled Tasks - SFD - after 24.10
+    Get-ScheduledTask -TaskPath "\Workspace ONE\SfdAgent\*" | Unregister-ScheduledTask  -Confirm:$false
 
-#Clean Scheduled Tasks - SFD
-Get-ScheduledTask -TaskPath "\Microsoft\Windows\EnterpriseMgmt\$($EnrollmentKey)\*" | Unregister-ScheduledTask  -Confirm:$false
+    $scheduleObject = New-Object -ComObject Schedule.Service
+    $scheduleObject.connect()
+    $rootFolder = $scheduleObject.GetFolder("\Workspace ONE")
+    $rootFolder.DeleteFolder("SfdAgent", $null)
+    
 
-$scheduleObject = New-Object -ComObject Schedule.Service
-$scheduleObject.connect()
-$rootFolder = $scheduleObject.GetFolder("\Microsoft\Windows\EnterpriseMgmt")
-$rootFolder.DeleteFolder("$($EnrollmentKey)", $null)
+    #Delete user certificates
+    $UserCerts = Get-ChildItem cert:"CurrentUser" -Recurse
+    $UserCerts | Where-Object { $_.Issuer -like "*AirWatch*" -or $_.Issuer -like "*AwDeviceRoot*" } | Remove-Item -Force
 
-#Clean Scheduled Tasks - SFD
-Get-ScheduledTask -TaskPath "\vmware\SfdAgent\*" | Unregister-ScheduledTask  -Confirm:$false
-
-$scheduleObject = New-Object -ComObject Schedule.Service
-$scheduleObject.connect()
-$rootFolder = $scheduleObject.GetFolder("\vmware")
-$rootFolder.DeleteFolder("SfdAgent", $null)
- 
-#Delete user certificates
-$UserCerts = Get-ChildItem cert:"CurrentUser" -Recurse
-$UserCerts | Where-Object { $_.Issuer -like "*AirWatch*" -or $_.Issuer -like "*AwDeviceRoot*" } | Remove-Item -Force
-
-#Delete device certificates
-$DeviceCerts = Get-ChildItem cert:"LocalMachine" -Recurse
-$DeviceCerts | Where-Object { $_.Issuer -like "*AirWatch*" -or $_.Issuer -like "*AwDeviceRoot*" } | Remove-Item -Force
-
+    #Delete device certificates
+    $DeviceCerts = Get-ChildItem cert:"LocalMachine" -Recurse
+    $DeviceCerts | Where-Object { $_.Issuer -like "*AirWatch*" -or $_.Issuer -like "*AwDeviceRoot*" } | Remove-Item -Force
 
 #Install Workspace One Agent
 if ($Reenrolldevice -eq "true") {
